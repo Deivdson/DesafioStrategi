dee05e0fc6fcb0fed57b1029373b8b95b555f06e
daylescheduletads <daylescheduletads@gmail.com>
2024-01-04 16:46:14 -0300

    docker compose

--- backend/api/tests/test__models.py
+++ backend/api/tests/test__models.py
@@ -13,16 +13,17 @@ def test_create_auth():
 def test_create_group():
     group_x = group.Group('name', 'description', '1')
 
-    assert( 
+    assert(
         group_x.name == 'name' and
         group_x.description == 'description' and
         group_x.user_id == '1'
     )
 
 def test_create_hero():
-    hero_x = hero.Hero('name', 'description', 'thumbnail')
+    hero_x = hero.Hero('1', 'name', 'description', 'thumbnail')
 
     assert(
+        hero_x.id == '1' and
         hero_x.name == 'name' and
         hero_x.description == 'description' and
         hero_x.thumbnail == 'thumbnail'
--- backend/api/utils/load_heros.py
+++ backend/api/utils/load_heros.py
@@ -9,17 +9,15 @@ from api.utils.service import fetch_api as fetch_api
 def load_heros():
     limit = 100
     offset = 0
-    print('load heros')
+    print('load heros...')
     for i in range(9):
         data = fetch_api(f'https://gateway.marvel.com/v1/public/characters?limit={limit}&offset={offset}')
         print(i)   
         list_heros = []
         for caracter in data:            
             exist = Hero.query.filter_by(name=caracter['name']).first()
-            if not exist and caracter:
-                new_hero = {'name':caracter['name'], 'description':caracter['description'], 'thumbnail':caracter['thumbnail'].get('path')}
-                print('-> ',new_hero,'\n')
-                hero = Hero(id=caracter['id'], name=caracter['name'], description=caracter['description'], thumbnail=caracter['thumbnail'].get('path'))
+            if not exist and caracter:             
+                hero = Hero(id=caracter.get('id'), name=caracter.get('name'), description=caracter.get('description'), thumbnail=caracter.get('thumbnail').get('path'))
                 list_heros.append(hero)
         offset += limit
             
